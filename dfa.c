8105908b08151dc91a13a6e108c2230a32c0837a
pbisen <bisenparth15@gmail.com>
2022-02-26 16:28:43 +0530

    dfa added but retract and function pointer still to be done

--- dfa.c
+++ dfa.c
@@ -58,11 +58,118 @@ int main(){
             case '~':
                 state = 44;
                 break;
+            case '&':
+                state = 45;
+                break;
+            case '@':
+                state = 48;
+                break;
+            case '>':
+                state = 51;
+                break;
+            case '=':
+                state = 54;
+                break;
+            case '!':
+                state = 56;
+                break;
             default:
                 break;
             }
         }
         break;
+    case 28:
+        c = get_char_From_buffer(fp);      // plaacholder for now
+        if(c == '='){
+            state = 29;
+            break;
+        }
+        else if(c == '-')(
+            state = 30;
+        )
+        else{
+            state = 33;
+            break;
+        }
+    case 30:
+        c = get_char_From_buffer(fp);
+        if(c == '-'){
+            state = 31;
+        }
+        else{
+            break;
+        }
+    case 31:
+        c = get_char_From_buffer(fp);
+        if(c == '-'){
+            state = 32;
+            break;
+        }
+        else{
+            break;
+        }
+    case 45:
+        c = get_char_From_buffer(fp);
+        if(c == '&'){
+            state = 46;
+        }
+        else{
+            break;
+        }
+    case 46:
+        c = get_char_From_buffer(fp);
+        if(c == '&'){
+            state = 47;
+            break;
+        }
+        else{
+            break;
+        }
+    case 48:
+        c = get_char_From_buffer(fp);
+        if(c == '@'){
+            state = 49;
+        }
+        else{
+            break;
+        }
+    case 49:
+        c = get_char_From_buffer(fp);
+        if(c == '@'){
+            state = 50;
+            break;
+        }
+        else{
+            break;
+        }
+    case 51:
+        c = get_char_From_buffer(fp);
+        if(c == '='){
+            state = 53;
+            break;
+        }
+        else{
+            state = 52;
+            break;
+        }
+    case 54:
+        c = get_char_From_buffer(fp);
+        if(c == '='){
+            state = 55;
+            break;
+        }
+        else{
+            break;
+        }
+    case 56:
+        c = get_char_From_buffer(fp);
+        if(C == '='){
+            state = 57;
+            break;
+        }
+        else{
+            break;
+        }
     
     default:
         break;
