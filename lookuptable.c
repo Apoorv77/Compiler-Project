609b91a65210f52e43331743c29747e8fbb0293a
Parth <65162182+pbisen@users.noreply.github.com>
2022-02-27 12:01:55 +0530

    working lookuptable w\ main demo

--- lookuptable.c
+++ lookuptable.c
@@ -1,5 +1,4 @@
 #include "lookuptable.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -22,47 +21,70 @@ void init_lookuptable(lookuptable_element* table){
         printf("HashTable allocation not found,Creating One.\n");
     }
     for(int i = 0; i < HASHTABLE_SIZE; i++){
-        table[i].token_value = "";
+        // strcpy(table[i].token_value, "");
         table[i].added = false;
     //   printf("checking loop %d\n", i);
     }
     
-  //  printf("Table Initialized\n");
+    printf("Table Initialized\n");
+
+    add_tokens();
+
+}
+
+void add_tokens(){
+    insert_token(lookuptable, "with", "TK_WITH");
+    insert_token(lookuptable, "parameters", "TK_PARAMETERS");
+    insert_token(lookuptable, "end", "TK_END");
+    insert_token(lookuptable, "While", "TK_WHILE");
+    insert_token(lookuptable, "Union", "TK_UNION");
+    insert_token(lookuptable, "endunion", "TK_ENDUNION");
+    insert_token(lookuptable, "as", "TK_AS");
+    insert_token(lookuptable, "Type", "TK_TYPE");
+    insert_token(lookuptable, "_main", "TK_MAIN");
+    insert_token(lookuptable, "Global", "TK_GLOBAL");
+    insert_token(lookuptable, "parameter", "TK_PARAMETER");
+    insert_token(lookuptable, "List", "TK_LIST");
+    insert_token(lookuptable, "Input", "TK_INPUT");
+    insert_token(lookuptable, "Output", "TK_OUTPUT");
+    insert_token(lookuptable, "Int", "TK_INT");
+    insert_token(lookuptable, "Real", "TK_REAL");
+    insert_token(lookuptable, "endwhile", "TK_ENDWHILE");
+    insert_token(lookuptable, "If", "TK_IF");
+    insert_token(lookuptable, "Then", "TK_THEN");
+    insert_token(lookuptable, "Endif", "TK_ENDIF");
+    insert_token(lookuptable, "Read", "TK_READ");
+    insert_token(lookuptable, "Write", "TK_WRITE");
+    insert_token(lookuptable, "Return", "TK_RETURN");
+    insert_token(lookuptable, "Call", "TK_CALL");
+    insert_token(lookuptable, "Record", "TK_RECORD");
+    insert_token(lookuptable, "endrecord", "TK_ENDRECORD");
+    insert_token(lookuptable, "Else", "TK_ELSE");
+    insert_token(lookuptable, "definetype", "TK_DEFINETYPE");
 }
 
 //Insert
 void insert_token(lookuptable_element* table,char* lexeme, char* tokenVal){
-   // printf("Inside insert token function\n");
     int lexeme_hash=hash(lexeme);
     int probe=1;
-   // printf("Lexeme Hashed: %d\n", lexeme_hash);
     while(table[lexeme_hash].added){
-        if(strcmp(table[lexeme_hash].token_value,lexeme) == 0){
-            break;
-         //   printf("PreExisting Token found \n");
-        }
-
         lexeme_hash = (lexeme_hash + (probe*probe)) % HASHTABLE_SIZE;
         probe++;
     }
-
-    //printf("Empty Space Found\n");
-    table[lexeme_hash].token_value = tokenVal;
+    strcpy(table[lexeme_hash].lexeme, lexeme);
+    strcpy(table[lexeme_hash].token_value, tokenVal);
     table[lexeme_hash].added=true;
-   // printf("Token Added\n");
+    printf("Token Added: Lexeme: %s\t\t\tTokenValue: %s\n", lexeme, tokenVal);
 }
 
 //Search the hash table
 char* find_token(lookuptable_element* table, char* lexeme){
-   // printf("Inside find_token\n");
 
     int lexeme_hash = hash(lexeme);
     int probe = 1;
 
-   // printf("Lexeme Hashed: %d\n", lexeme_hash);
     while(table[lexeme_hash].added){
-        if(strcmp(table[lexeme_hash].token_value, lexeme) == 0){
-           // printf("Found\n");
+        if(strcmp(table[lexeme_hash].lexeme, lexeme) == 0){
             return (table[lexeme_hash].token_value);
         }
         else{
@@ -71,10 +93,15 @@ char* find_token(lookuptable_element* table, char* lexeme){
         }
     }
 
-    return NULL;
+    return "TK_FIELDID";
 }
 
-//add the next part to the actual file
+int main(){
+    init_lookuptable(lookuptable);
+    printf("Queried: %s \t\t Returned: %s\n", "While", find_token(lookuptable, "While"));
+    printf("Queried: %s \t\t Returned: %s\n", "definetype", find_token(lookuptable, "definetype"));
+    printf("Queried: %s \t\t Returned: %s\n", "hehe", find_token(lookuptable, "hehe"));
+}
 
 
 
--- lookuptable.h
+++ lookuptable.h
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #define LEXEME_LEN_LIMIT 50
 #define HASHTABLE_SIZE 1000000
 
 typedef struct lookuptable_element{
-    char *token_value;
+    char lexeme[50];
+    char token_value[50];
     bool added;
 } lookuptable_element;
 
 lookuptable_element lookuptable[HASHTABLE_SIZE];
 
 int hash(char *str);
-void init_lookuptable(lookuptable_element* table);
+void  init_hash_table(lookuptable_element* table);
 void insert_token(lookuptable_element* table,char* lexeme, char* tokenVal);
 char* find_token(lookuptable_element* table, char* lexeme);
+void add_tokens();
 
