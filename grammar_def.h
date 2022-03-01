3339ecc9ee34a1f61246a65d92cfb107f690333e
amishbhat <53873666+amishbhat@users.noreply.github.com>
2022-03-01 23:34:30 +0530

    Add files via upload

--- grammar_def.h
+++ grammar_def.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+# define MAX_SIZE_OF_SYMBOL 50
 /*
 The definitions of NonTerminal and Terminal need to be pasted here.
 
@@ -9,39 +11,174 @@ typedef enum{
 typedef enum{
     stuff
 }NonTerminal;
- */
+*/
+typedef enum{
+    program,
+	mainFunction,
+	otherFunctions,
+	function,
+	input_par,
+    output_par,
+    parameter_list,
+    dataType,
+    primitiveDatatype,
+    constructedDatatype,
+    remaining_list,
+    stmts,
+    typeDefinitions,
+    typeDefinition,
+    fieldDefinitions,
+    fieldDefinition,
+    moreFields,
+    declarations,
+    declaration,
+    global_or_not,
+    otherStmts,
+    stmt,
+    assignmentStmt,
+    singleOrRecId,
+    tempSoRId,
+    funCallStmt,
+    outputParameters,
+    inputParameters,
+    iterativeStmt,
+    conditionalStmt,
+    elsePart,
+    ioStmt,
+    arithmeticExpression,
+    arithExp1,
+    term,
+    arithExp2,
+    oper1,
+    oper2,
+    factor,
+    booleanExpression,
+    var,
+    allVar, 
+    temp,
+    logicalOp,
+    relationalOp,
+    returnStmt,
+    optionalReturn,
+    idList,
+    more_ids
+}NonTerminal;
+
+typedef enum{
+	TK_ASSIGNOP,
+	TK_COMMENT,
+	TK_FIELDID,
+	TK_ID,
+	TK_NUM,
+	TK_RNUM,
+	TK_FUNID,
+	TK_RECORDID,
+	TK_WITH,
+	TK_PARAMETERS,
+	TK_END,
+	TK_WHILE,
+	TK_TYPE,
+	TK_MAIN,
+	TK_GLOBAL,
+	TK_PARAMETER,
+	TK_LIST,
+	TK_SQL,
+	TK_SQR,
+	TK_INPUT,
+	TK_OUTPUT,
+	TK_INT,
+	TK_REAL,
+	TK_COMMA,
+	TK_SEM,
+	TK_COLON,
+	TK_DOT,
+	TK_ENDWHILE,
+	TK_OP,
+	TK_CL,
+	TK_IF,
+	TK_THEN,
+	TK_ENDIF,
+	TK_READ,
+	TK_WRITE,
+	TK_RETURN,
+	TK_PLUS,
+	TK_MINUS,
+	TK_MUL,
+	TK_DIV,
+	TK_CALL,
+	TK_RECORD,
+	TK_ENDRECORD,
+	TK_ELSE,
+	TK_AND,
+	TK_OR,
+	TK_NOT,
+	TK_LT,
+	TK_LE,
+	TK_EQ,
+	TK_GT,
+	TK_GE,
+	TK_NE,
+	EPSILON,
+	DOLLAR,
+	ERROR,
+	BOOLEAN,
+	LABEL,
+	GOTO
+}Terminal;
+
 #define sizeOfGrammer 45 //this is the number of grammmar rules in the grammar given by maam.
 
 // we have to put -1 in one of the two in the symbol struct. Symbol can be either a terminal or a non terminal
-struct symbol{
-    NonTerminal nonterminal;
-    Terminal terminal;
-};
+// struct symbol{
+//     NonTerminal nonterminal;
+//     Terminal terminal;
+// };
 // random testing
 /*
 // typedef struct symbol symbol;
 // struct symbol sym;
 // sym.nonterminal = (NonTerminal)var;
 */
+//this is one list of symbols that will have one grammar rule 
+// struct symbol_list{
+//     NonTerminal nonterminal;
+// 	Terminal terminal;
+// 	bool isTerm;
+//     struct symbol_list* next;
+// };
 
-struct symbol_list{
-    struct symbol sym;
-    struct symbol_list* next;
-    bool isTerm;
-};
+// struct rules{
+//     struct symbol_list* rule_head;
+//     struct rules* next;
+// };
 
-struct symbol_list_head{
-    struct symbol_list* head;
-    int size;
-};
+// struct rule{
+//     struct rules* header;
+//     int size;
+// };
 
-struct rules{
-    struct symbol_list_head* rule_head;
-    struct rules* next;
-};
+// struct lhs{
+// 	NonTerminal nonterminal;
+// 	struct rules array[10];
+// }
+// // struct lhs* lhs_array;
+// struct rule** grammar;
 
-struct rule{
-    struct rules* header;
-    int size;
-};
-struct rule* grammar[sizeOfGrammer];
+typedef struct Rhs{
+	char token[50];
+	int isNonTerm; // 1 = terminal && 0 = nonterminal 
+	struct Rhs* next;
+}rhs;
+
+// typedef struct Rhs_head{
+// 	rhs* head;
+// 	int size;
+// }rhs_head;
+
+typedef struct Lhs{
+	char nonterminal[50];
+	rhs* rule[10]; // 10 is the maximum or's that we can have in a grammar rule.
+}lhs;
+
+lhs* grammar;
+
---
+++ grammar_rules_new.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grammar_def.h"
+#define sizeOfGrammer 45
+#define MAX_SIZE_OF_SYMBOL 50
+
+lhs* take_input_from_grammar_file(FILE* ptr)
+{
+    lhs* grammar = ( lhs*)malloc(sizeof(lhs)*sizeOfGrammer);
+    int rule_no = 0;
+    char ch = 'a';
+    while(ch!=EOF)
+    {
+        ch = fgetc(ptr);
+        // printf("%c",ch);
+        if(ch==EOF)
+        break;
+        // printf("%c",ch);
+        // this if condition takes care of the lhs part
+        if(ch=='<')
+        {
+            // printf("%c",ch);
+            char string[50];
+            ch = fgetc(ptr);
+            int i = 0;
+            while(ch!='>')
+            {
+                string[i++] = ch;
+                ch = fgetc(ptr);
+            }
+            string[i++] = '\0';
+            // printf("%c",string[0]);
+            for(int j =0;string[j]!='\0';j++)
+            {
+                
+                grammar[rule_no].nonterminal[j] = string[j];
+                // printf("%c",grammar[rule_no].nonterminal[j]);
+            }
+            ch = fgetc(ptr);
+        }
+        while(ch!='\n'||ch!=EOF)
+        {
+            // printf("%c",ch);
+            int order = 0;
+            int or_no = 0;
+            // rhs* temp;
+            if(ch=='<')
+            {
+                char string[MAX_SIZE_OF_SYMBOL];
+                // char* symbol = (char*)malloc(sizeof(char)*MAX_SIZE_OF_SYMBOL);
+                for(int i =0;i<MAX_SIZE_OF_SYMBOL;i++)
+                {
+                    ch = fgetc(ptr);
+                    // printf("%c",ch);
+                    if(ch=='>')
+                    {rhs* temp;
+                        string[i] = '\0';
+                        if(order==0)
+                        {  
+                            grammar[rule_no].rule[or_no] = (rhs*)malloc(sizeof(rhs));
+                            temp = grammar[rule_no].rule[or_no];
+                        }
+                        for(int j =0;string[j]!='\0';j++)
+                        temp->token[j] = string[j]/*,printf("%c",string[j])*/;
+                        temp->isNonTerm = 0;                        
+                        temp->next = (rhs*)malloc(sizeof(rhs));
+                        temp = temp->next;
+                        order++;
+                        ch = fgetc(ptr);// to move the pointer forward so that in the next iteration, it can work properly and like go into the correct if conds
+                        break;
+                    }
+                    else
+                    string[i] = ch;
+                }
+            }
+            else if(ch=='T')//assuming all tokens will start with a TK_
+            {
+                char string[MAX_SIZE_OF_SYMBOL];
+                // char* symbol = (char*)malloc(sizeof(char)*MAX_SIZE_OF_SYMBOL);
+                for(int i=0;i<MAX_SIZE_OF_SYMBOL;i++)
+                {
+                    if((ch<'A'||ch>'Z')&&ch!='_')
+                    {
+                        // if(ch=='\n')
+                        // printf("HELLO");
+                        // printf("%c",ch);
+                        string[i] = '\0';
+                         rhs* temp;
+                        if(order==0)
+                        { 
+                            grammar[rule_no].rule[or_no] = (rhs*)malloc(sizeof(rhs));
+                            temp = grammar[rule_no].rule[or_no];
+                        }
+                        for(int j =0;string[j]!='\0';j++)
+                        temp->token[j] = string[j]/*,printf("%c",string[j])*/;
+                        temp->isNonTerm = 1;                        
+                        temp->next = (rhs*)malloc(sizeof(rhs));
+                        temp = temp->next;
+                        order++;
+                        // put "symbol" string in the correct place in grammar 
+                        // it is surely a Terminal. So do it accordingly.
+                        // it will matter if ch == '|' or '<'
+                        // in the first case, a new "rules" node has to be made and the next thing put into that
+                        // in the second case, simple continuation of the same linked list
+                        //
+                        // ch = fgetc(ptr);
+                        break;
+                    }
+                    else
+                    {
+                        string[i] = ch;
+                        // ch = fgetc(ptr);
+                    }
+                    ch = fgetc(ptr);
+                }
+
+            }
+            else if(ch=='=')
+            {
+                while(ch!='<'&&ch!='T'&&ch!='e')// e is epsilon
+                {
+                    // printf("%c",ch);
+                    ch = fgetc(ptr);
+                }
+            }
+            else if(ch=='|')
+            {
+                // temp = NULL;
+                or_no++;
+                ch = fgetc(ptr);
+            }
+            else if(ch==' ')
+            {
+                while(ch==' ')
+                {
+                    ch = fgetc(ptr);
+                }
+            }
+            if(ch=='\n'||ch==EOF)
+                break;
+           
+            // i have to handle the condition when ">|" comes together like that. I can handle it in the first case itself imo. 
+        }
+        rule_no++;
+        // printf("%d",rule_no);
+    }
+    return grammar;
+}
+
+void main()
+{
+    FILE* input = fopen("grammar_test_file.txt", "r");
+    if(input==NULL){
+        fprintf(stderr, "Error Opening Grammar File\n");
+        return;
+    }
+    lhs* input_grammar = take_input_from_grammar_file(input);
+    printf("%s", input_grammar[1].rule[0]->isNonTerm);
+}
